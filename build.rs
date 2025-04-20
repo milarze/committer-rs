9bff74f9aa878c1145f64354518544844d030601
Ezra Lim <ezralim1992@gmail.com>
2025-04-21 00:59:40 +0800

    Updated the build

--- build.rs
+++ build.rs
@@ -3,26 +3,26 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-const HF_MODEL_ID: &str = "google-t5/t5-small";
+const HF_MODEL_ID: &str = "davidgaofc/TechDebtLabeler";
 
 const MODEL_FILES: &[&str] = &["config.json", "tokenizer.json", "model.safetensors"];
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let out_dir = env::var("OUT_DIR").context("Failed to get OUT_DIR")?;
-    let model_dir = Path::new(&out_dir).join("model");
-
-    fs::create_dir_all(&model_dir).context("Failed to create model directory")?;
-
-    let api = hf_hub::api::sync::Api::new()?;
-    let repo = api.model(HF_MODEL_ID.to_string());
-
-    for file in MODEL_FILES {
-        let src_path = repo.get(file)?;
-        let dest_path = model_dir.join(file);
-        fs::copy(src_path, &dest_path)
-            .with_context(|| format!("Failed to copy {} to {}", file, dest_path.display()))?;
-    }
-
-    println!("cargo:rerun-if-changed=build.rs");
+    //    let out_dir = env::var("OUT_DIR").context("Failed to get OUT_DIR")?;
+    //    let model_dir = Path::new(&out_dir).join("model");
+    //
+    //    fs::create_dir_all(&model_dir).context("Failed to create model directory")?;
+    //
+    //    let api = hf_hub::api::sync::Api::new()?;
+    //    let repo = api.model(HF_MODEL_ID.to_string());
+    //
+    //    for file in MODEL_FILES {
+    //        let src_path = repo.get(file)?;
+    //        let dest_path = model_dir.join(file);
+    //        fs::copy(src_path, &dest_path)
+    //            .with_context(|| format!("Failed to copy {} to {}", file, dest_path.display()))?;
+    //    }
+    //
+    //    println!("cargo:rerun-if-changed=build.rs");
     Ok(())
 }
--- src/generators/embedded.rs
+++ src/generators/embedded.rs
@@ -1,5 +1,3 @@
-pub const MODEL_SAFE_TENSORS: &[u8] =
-    include_bytes!(concat!(env!("OUT_DIR"), "/model/model.safetensors"));
-pub const CONFIG_JSON: &str = include_str!(concat!(env!("OUT_DIR"), "/model/config.json"));
-pub const TOKENIZER_JSON: &str =
-    include_str!(concat!(env!("OUT_DIR"), "/model/tokenizer.json"));
+pub const MODEL_SAFE_TENSORS: &[u8] = include_bytes!("../../model/model.safetensors");
+pub const CONFIG_JSON: &str = include_str!("../../model/config.json");
+pub const TOKENIZER_JSON: &str = include_str!("../../model/tokenizer.json");
